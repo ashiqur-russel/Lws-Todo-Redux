452dbf99c282c5e8b1a18ab07c458dc213094d23
ashiqurras <ashiqurrussel2@gmail.com>
2023-02-25 16:17:17 +0100

    actions,actionTypes and reducer added

---
+++ src/redux/todos/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADDED = "todos/added";
+export const TOGGLED = "todos/toggled";
+export const DELETED = "todos/deleted";
+export const COLORSELECTED = "todos/colorselected";
+export const ALLCLEARED = "todos/allcleared";
+export const ALLCOMPLETED = "todos/allcompleted";
