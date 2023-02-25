a3c2a6b69382588fd375a0928e6f34a884d71782
ashiqurras <ashiqurrussel2@gmail.com>
2023-02-25 15:08:28 +0100

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div class="fixed top-0 left-0 text-center w-full header bg-gray-600 py-4 text-white font-bold text-lg shadow-lg">
+      Todo Application with Redux
+    </div>
+  );
+};
+
+export default Navbar;
