fd5195a8e28b43f8d72eb174583b23f861858d9b
ashiqurras <ashiqurrussel2@gmail.com>
2023-02-26 20:14:27 +0100

    todo reducer all case

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,13 @@
+export const initialState = [
+  {
+    id: 1,
+    text: "Learn Redux",
+    completed: true,
+  },
+  {
+    id: 2,
+    text: "Learn JS",
+    completed: false,
+    color: "yellow",
+  },
+];
